f13c036294039d09f1fe8219463d9ff267e624d2
Annett Hossain <annetthossain@outlook.com>
2025-11-08 20:01:30 +0600

    redesign

---
+++ src/cmdline.hxx
@@ -0,0 +1,23 @@
+#pragma once
+
+enum cmd_opts {
+  Help,
+  Version,
+  O1,
+  O2,
+  O3,
+  I,
+  L,
+  c,
+  S,
+  Si,
+};
+typedef cmd_opts cmd_opts_t;
+
+class cmd_args {
+public:
+
+private:
+
+};
+
